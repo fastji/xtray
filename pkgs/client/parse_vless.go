1d0e3cd1937c747f5143a26aba425ded7772c109
moqsien <moqsien@foxmail.com>
2023-04-25 10:49:46 +0800

    vless

--- pkgs/client/parse_vless.go
+++ pkgs/client/parse_vless.go
@@ -1,5 +1,13 @@
 package client
 
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+
+	"github.com/gogf/gf/encoding/gjson"
+)
+
 /*
 	"vnext": [{
 		"address": "example.com",
@@ -26,18 +34,51 @@ var VlessStr string = `{
 }`
 
 type VlessOutbound struct {
-	Address  string
-	Port     int
-	UserId   string
-	Security string
+	Address    string
+	Port       int
+	UserId     string
+	Security   string
+	Encryption string
+	Type       string
+	Path       string
+	Raw        string
 }
 
 /*
 vless://b1e41627-a3e9-4ebd-9c92-c366dd82b13f@xray.ibgfw.top:2083?encryption=none&security=tls&type=ws&host=&path=/wSXCvstU/#xray.ibgfw.top%3A2083
 */
-func (that *VlessOutbound) parse(rawUri string) {}
+func (that *VlessOutbound) parse(rawUri string) {
+	that.Raw = rawUri
+	if r, err := url.Parse(rawUri); err == nil && r.Scheme == "vless" {
+		that.Address = r.Hostname()
+		that.Port, _ = strconv.Atoi(r.Port())
+		that.UserId = r.User.Username()
+		that.Security = r.Query().Get("security")
+		that.Encryption = r.Query().Get("encryption")
+		that.Type = r.Query().Get("type")
+		that.Path = r.Query().Get("path")
+	}
+}
 
 func (that *VlessOutbound) GetConfigStr(rawUri string) string {
 	that.parse(rawUri)
-	return ""
+	j := gjson.New(VlessStr)
+	j.Set("vnext.0.address", that.Address)
+	j.Set("vnext.0.port", that.Port)
+	j.Set("vnext.0.users.0.id", that.UserId)
+	j.Set("vnext.0.users.0.encryption", that.Encryption)
+	vnextStr := j.MustToJsonIndentString()
+	j = gjson.New(StreamStr)
+	j.Set("network", that.Type)
+	j.Set("security", that.Security)
+	j.Set("wsSettings.path", that.Path)
+	streamStr := j.MustToJsonIndentString()
+	confStr := fmt.Sprintf(XrayConfStr, vnextStr, streamStr)
+	j = gjson.New(confStr)
+	j.Set("outbounds.0.protocol", "vless")
+	return j.MustToJsonIndentString()
+}
+
+func (that *VlessOutbound) GetRawUri() string {
+	return that.Raw
 }
