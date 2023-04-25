2ad190913e1176eec6fd34f369a3d1724ff75ff4
moqsien <moqsien@foxmail.com>
2023-04-25 12:50:06 +0800

    trojan

--- main.go
+++ main.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"net/url"
 )
 
 func main() {
-	u := "188.119.65.143:12337:origin:rc4:plain:bG5jbi5vcmcgbjh0/?obfsparam=&remarks=5L-E572X5pavTw&group=TG5jbi5vcmc"
-	// r, _ := url.Parse(u)
-	// if r == nil {
-	// 	return
-	// }
-	// fmt.Println(r.Scheme)
-	// fmt.Println(r.Hostname())
-	// fmt.Println(r.Port())
-	// fmt.Println(r.User.Username())
-	// fmt.Println(r.User.Password())
-	// fmt.Println(r.Query().Get("remarks"))
-	// fmt.Println(r.Query().Get("group"))
-	v := strings.SplitN(u, ":", 3)
-	fmt.Println(v[0])
-	fmt.Println(v[1])
-	fmt.Println(v[2])
+	u := "trojan://b5e4e360-5946-470b-aad0-db98f50faa57@frontend.yijianlian.app:54430?security=tls&type=tcp&headerType=none#%F0%9F%87%BA%F0%9F%87%B8%20Relay%20%F0%9F%87%BA%F0%9F%87%B8%20United%20States%2011%20TG%3A%40SSRSUB"
+	r, _ := url.Parse(u)
+	if r == nil {
+		return
+	}
+	fmt.Println(r.Scheme)
+	fmt.Println(r.Hostname())
+	fmt.Println(r.Port())
+	fmt.Println(r.User.Username())
+	fmt.Println(r.User.Password())
+	fmt.Println(r.Query().Get("security"))
+	fmt.Println(r.Query().Get("type"))
 }
--- pkgs/client/parse_trojan.go
+++ pkgs/client/parse_trojan.go
@@ -1,5 +1,14 @@
 package client
 
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/gogf/gf/encoding/gjson"
+)
+
 /*
 	"servers": [{
 		"address": "127.0.0.1",
@@ -24,4 +33,45 @@ type TrojanOutboud struct {
 	Port     int
 	Password string
 	Email    string
+	Security string
+	Type     string
+	Path     string
+}
+
+/*
+trojan://b5e4e360-5946-470b-aad0-db98f50faa57@frontend.yijianlian.app:54430?security=tls&type=tcp&headerType=none#%F0%9F%87%BA%F0%9F%87%B8%20Relay%20%F0%9F%87%BA%F0%9F%87%B8%20United%20States%2011%20TG%3A%40SSRSUB
+*/
+func (that *TrojanOutboud) parse(rawUri string) {
+	if strings.HasSuffix(rawUri, "trojan://") {
+		if u, err := url.Parse(rawUri); err == nil {
+			that.Address = u.Hostname()
+			that.Port, _ = strconv.Atoi(u.Port())
+			that.Password = u.User.Username()
+			that.Security = u.Query().Get("security")
+			that.Type = u.Query().Get("type")
+			that.Path = u.Query().Get("path")
+		}
+	}
+}
+
+func (that *TrojanOutboud) GetConfigStr(rawUri string) (r string) {
+	that.parse(rawUri)
+	j := gjson.New(TrojanStr)
+	j.Set("servers.0.address", that.Address)
+	j.Set("servers.0.port", that.Port)
+	j.Set("servers.0.password", that.Password)
+	j.Set("servers.0.email", that.Email)
+	serverStr := j.MustToJsonIndentString()
+	streamStr := "{}"
+	if that.Type != "" {
+		j = gjson.New(StreamStr)
+		j.Set("network", that.Type)
+		j.Set("security", that.Security)
+		j.Set("wsSettings.path", that.Path)
+		streamStr = j.MustToJsonIndentString()
+	}
+	confStr := fmt.Sprintf(XrayConfStr, serverStr, streamStr)
+	j = gjson.New(confStr)
+	j.Set("outbounds.0.protocol", "trojan")
+	return j.MustToJsonIndentString()
 }
